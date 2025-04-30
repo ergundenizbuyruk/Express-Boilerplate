03b1ad1babcd4f171ae3b023be08ff33d91cbef3
ergundenizbuyruk <ergunndenizbuyruk@gmail.com>
2025-04-30 13:31:17 +0300

    feat: wrap controller methods with asyncHandler for improved error handling

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,25 +1,30 @@
 import { Request, Response } from "express";
 import authService from "../services/auth-service/auth.service";
+import asyncHandler from "express-async-handler";
 
-export const login = async (req: Request, res: Response) => {
+export const login = asyncHandler(async (req: Request, res: Response) => {
   const loginRes = await authService.login(req.body);
   res.status(loginRes.statusCode).json(loginRes);
-};
+});
 
-export const loginWithRefreshToken = async (req: Request, res: Response) => {
-  const loginRes = await authService.loginWithRefreshToken(req.body);
-  res.status(loginRes.statusCode).json(loginRes);
-};
+export const loginWithRefreshToken = asyncHandler(
+  async (req: Request, res: Response) => {
+    const loginRes = await authService.loginWithRefreshToken(req.body);
+    res.status(loginRes.statusCode).json(loginRes);
+  }
+);
 
-export const revokeRefreshToken = async (req: Request, res: Response) => {
-  const revokeRes = await authService.revokeRefreshToken(req.body);
-  res.status(revokeRes.statusCode).json(revokeRes);
-};
+export const revokeRefreshToken = asyncHandler(
+  async (req: Request, res: Response) => {
+    const revokeRes = await authService.revokeRefreshToken(req.body);
+    res.status(revokeRes.statusCode).json(revokeRes);
+  }
+);
 
-export const register = async (req: Request, res: Response) => {
+export const register = asyncHandler(async (req: Request, res: Response) => {
   const userDto = await authService.register(req.body);
   res.status(userDto.statusCode).json(userDto);
-};
+});
 
 export default {
   login,
--- src/controllers/role-controller.ts
+++ src/controllers/role-controller.ts
@@ -1,30 +1,31 @@
 import { Request, Response } from "express";
 import roleService from "../services/role-service/role.service";
+import asyncHandler from "express-async-handler";
 
-export const getAll = async (req: Request, res: Response) => {
+export const getAll = asyncHandler(async (req: Request, res: Response) => {
   const users = await roleService.getAll();
   res.status(users.statusCode).json(users);
-};
+});
 
-export const get = async (req: Request, res: Response) => {
+export const get = asyncHandler(async (req: Request, res: Response) => {
   const user = await roleService.get(req.params.id);
   res.status(user.statusCode).json(user);
-};
+});
 
-export const create = async (req: Request, res: Response) => {
+export const create = asyncHandler(async (req: Request, res: Response) => {
   const newUser = await roleService.create(req.body);
   res.status(newUser.statusCode).json(newUser);
-};
+});
 
-export const update = async (req: Request, res: Response) => {
+export const update = asyncHandler(async (req: Request, res: Response) => {
   const updatedUser = await roleService.update(req.params.id, req.body);
   res.status(updatedUser.statusCode).json(updatedUser);
-};
+});
 
-export const remove = async (req: Request, res: Response) => {
+export const remove = asyncHandler(async (req: Request, res: Response) => {
   const result = await roleService.remove(req.params.id);
   res.status(result.statusCode).json(result);
-};
+});
 
 export default {
   getAll,
